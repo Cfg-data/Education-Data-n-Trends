36cace867ff5bfcdf49a649417b45bd450fc22de
Cfg-data <ceciliafreitasguerreiro@hotmail.com>
2024-12-19 14:41:57 +0000

    updates
    new app page
    file updates
    app page consolidation

--- app_files/app.py
+++ app_files/app.py
@@ -2,7 +2,7 @@
 
 import streamlit as st
 from data_loader import load_data
-from pages import opener, introduction, eda_results, ml_results
+from pages import opener, introduction, eda_results, ml_results, recommendations  # Import recommendations page
 
 # Set the page title and layout
 st.set_page_config(page_title="Education Project Overview", layout="wide")
@@ -12,7 +12,7 @@ data = load_data()
 
 # Display a sidebar with navigation options
 st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Opener", "Introduction", "EDA Results", "ML Results"])
+page = st.sidebar.radio("Go to", ["Opener", "Introduction", "EDA Results", "ML Results", "Recommendations"])  # Add Recommendations here
 
 # Display selected page content
 if page == "Opener":
@@ -23,3 +23,5 @@ elif page == "EDA Results":
     eda_results.show(data)
 elif page == "ML Results":
     ml_results.show(data)
+elif page == "Recommendations":  # Show Recommendations page
+    recommendations.show(data)
